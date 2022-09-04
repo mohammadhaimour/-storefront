585cfb807435d0cbd6456c2d4a4f4b32429ef0be
mohammadhaimour <mohammadhaimour1999@gmail.com>
2022-09-04 04:51:52 +0300

    part1

---
+++ demo/src/app.js
@@ -0,0 +1,11 @@
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+export default (props) => {
+    return (
+        <>
+            <Header />
+            <h3>App</h3>
+            <Footer />
+        </>
+    )
+}
---
+++ demo/src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+import App from "./app";
+const rootEl = document.getElementById("root");
+function Main() {
+    return (
+
+        <App />
+
+
+    )
+}
+// either <div> or <App />
+// ReactDOM.render(<div>hello</div>, rootEl);
+// instead of having <div>hello</div> we gonna put the App.js it will be like (replace line 2 with this)
+ReactDOM.render(<Main />, rootEl);
